d7639ae5d147c6358320dffab5b61a3312eba64c
Matej Drzik <wirrareka@timeflux.org>
2023-06-05 12:49:29 +0200

    5.2.10 android permissions fix

--- android/src/main/java/com/reactnativeproximiio/PermissionHelper.kt
+++ android/src/main/java/com/reactnativeproximiio/PermissionHelper.kt
@@ -2,14 +2,20 @@ package com.reactnativeproximiio
 
 import android.Manifest
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import io.proximi.proximiiolibrary.ProximiioAPI
 
 import com.facebook.react.modules.core.PermissionAwareActivity
 import com.facebook.react.modules.core.PermissionListener
+import io.proximi.proximiiolibrary.ProximiioAPI.BLUETOOTH_REQUEST
+import io.proximi.proximiiolibrary.ProximiioAPI.BLUETOOTH_REQUEST_S
 
 private const val TAG = "PermissionHelper"
 
@@ -22,9 +28,14 @@ class PermissionHelper {
   }
 
   fun checkAndRequest(activity: Activity, permissionListener: PermissionListener, force: Boolean = false) {
-    if (hasLocationPermission(activity) && isBluetoothEnabled()) {
-      return
+    if (hasLocationPermission(activity)) {
+      if (isBluetoothEnabled()) {
+        return
+      } else {
+        checkAndRequestBluetooth(activity)
+      }
     }
+
     if (!alreadyChecked || force) {
       alreadyChecked = true
       checkAndRequestLocationPermission(activity, permissionListener)
@@ -47,9 +58,45 @@ class PermissionHelper {
     }
   }
 
-  private fun checkAndRequestBluetooth(activity: Activity) {
-    if (!isBluetoothEnabled()) {
-      ActivityCompat.startActivityForResult(activity, Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), ProximiioAPI.BLUETOOTH_REQUEST, null)
+  /**
+   * Check if bluetooth is enabled and request to enable it if not.
+   */
+  private fun checkAndRequestBluetooth(mainActivity: Activity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+      if (ActivityCompat.checkSelfPermission(
+          mainActivity,
+          Manifest.permission.BLUETOOTH_SCAN
+        ) != PackageManager.PERMISSION_GRANTED
+      ) {
+        ActivityCompat.requestPermissions(
+          mainActivity,
+          arrayOf(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT
+          ),
+          BLUETOOTH_REQUEST_S
+        )
+        return
+      }
+
+      if (!isBluetoothEnabled()) {
+        ActivityCompat.startActivityForResult(
+          mainActivity,
+          Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),
+          BLUETOOTH_REQUEST,
+          null
+        )
+      }
+    } else {
+      if (!isBluetoothEnabled()) {
+        ActivityCompat.startActivityForResult(
+          mainActivity,
+          Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),
+          BLUETOOTH_REQUEST,
+          null
+        )
+        return
+      }
     }
   }
 
